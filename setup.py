90483ecc8d771284773b6934796b466e1234b83c
Waylon Flinn <waylonflinn@gmail.com>
2017-02-19 15:13:48 -0600

    update setup info for pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from distutils.core import setup
 setup(
   name = 'heapy',
   packages = ['heapy'], # this must be the same as the name above
-  version = '0.5',
-  description = 'An priority queue built with an in-place modifiable binary heap',
+  version = '0.6',
+  description = 'A priority queue built with an in-place modifiable binary heap',
   author = 'Waylon Flinn',
   author_email = 'waylonflinn@gmail.com',
   url = 'https://github.com/waylonflinn/heapy', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
   keywords = ['heap', 'priority queue', 'decrease-key', 'increase-key', 'dijkstra', 'rolling median'], # arbitrary keywords
   classifiers = [],
 )
